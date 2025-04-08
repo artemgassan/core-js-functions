6341e382c4c77bf965b1de03524a7c41a69d13e5
artemgassan <artemgassan@ya.ru>
2025-04-09 02:19:36 +0300

    feat: finish getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -226,8 +226,14 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let current = startFrom;
+
+  return function getIdFunction() {
+    const result = current;
+    current += 1;
+    return result;
+  };
 }
 
 module.exports = {
